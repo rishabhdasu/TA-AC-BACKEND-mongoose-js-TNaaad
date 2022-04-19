907e98d5d695d334b895a7c73dc5f9a2cf678e10
Rishabh Dasgupta <rishabhdasu@gmail.com>
2022-04-19 16:45:08 +0530

    Address Schema

--- block-BNaacf/sample/models/user.js
+++ block-BNaacf/sample/models/user.js
@@ -1,22 +1,14 @@
-var app = express();
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 
-var userSchema = new Schema({
-  name: String,
-  email: { type: String, uppercase: true },
-  age: { type: (Number = 0) },
-  age: { type: Number, default: 0 },
-  favorites: [String],
-  marks: [Number],
-});
-
-var addressSchema = new Schema({
-  village: String,
-  city: String,
-  state: String,
-  pin: Number,
-  user: Schema.Types.ObjectId,
-});
-
-app.listen(4000, () => {
-  console.log("Server is listening");
-});
+var userSchema = new Schema(
+  {
+    name: String,
+    email: { type: String, uppercase: true },
+    age: { type: (Number = 0) },
+    age: { type: Number, default: 0 },
+    favorites: [String],
+    marks: [Number],
+  },
+  { timestamps: true }
+);
